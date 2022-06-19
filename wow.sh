390db6a5573e072772fec26a4f3dc00e961540b5
BluelabelKiwi <100088903+BluelabelKiwi@users.noreply.github.com>
2022-06-19 15:29:00 +0300

    Add files via upload
    intro test

---
+++ wow.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo -e "\033[1;93m"
+echo "========================================================================"
+echo "    /II   /II  /IIIIII        /II   /II                 /II          ";
+echo "   | II  /II/ /II__  II      | III | II                | II          ";
+echo "   | II /II/ | II  \__/      | IIII| II  /IIIIII   /IIIIIII  /IIIIII ";
+echo "   | IIIII/  |  IIIIII       | II II II /II__  II /II__  II /II__  II";
+echo "   | II  II   \____  II      | II  IIII| II  \ II| II  | II| IIIIIIII";
+echo "   | II\  II  /II  \ II      | II\  III| II  | II| II  | II| II_____/";
+echo "   | II \  II|  IIIIII/      | II \  II|  IIIIII/|  IIIIIII|  IIIIIII";
+echo "   |__/  \__/ \______/       |__/  \__/ \______/  \_______/ \_______/";
+echo "========================================================================"
+echo -e "\e[0m"
+
+
+sleep 2
